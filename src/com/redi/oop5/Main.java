86ff90d78fb1535761e6ecd2feca255d4791bcdf
sys0p <horst@discardmail.com>
2021-04-29 13:44:14 +0200

    added some separators for easier to read printouts

--- src/com/redi/oop5/Main.java
+++ src/com/redi/oop5/Main.java
@@ -7,22 +7,30 @@ public class Main {
     public static void main(String[] args) {
 
         //exercise1
+        System.out.println("exercise 1");
         int[] testOut = returnUnique(createRandArray(5,10));
         for(int i : testOut){
             System.out.println(i);
         }
+        System.out.println("-----------");
 
         //exercise2
+        System.out.println("exercise 2");
         char[] uniqueChars = uniqueCharsInString("TESTOMAT");
         for(char c : uniqueChars){
             System.out.println(c);
         }
+        System.out.println("-----------");
 
         //exercise3
-        //System.out.println(intersection(createRandomHash(5, 9), createRandomHash(5, 9)));
+        System.out.println("exercise 3");
+        System.out.println(intersection(createRandomHash(5, 9), createRandomHash(5, 9)));
+        System.out.println("-----------");
 
         //exercise4
+        System.out.println("exercise 4");
         System.out.println(difference(createRandomHash(5, 9), createRandomHash(5, 9)));
+        System.out.println("-----------");
     }
 }
 
